497810c5f38a66a7a68689210fb1680d53f3696e
vinkels <yevinkesteijn@gmail.com>
2019-01-14 13:50:50 +0100

    push it real good

--- food.py
+++ food.py
@@ -14,21 +14,6 @@ class HiveModel(Model):
         self.foods = []
         self.init_food()
 
-    def init_food(self):
-        for i in range(self.start_food):
-            pos = (rd.randrange(self.width), rd.randrange(self.height))
-            util = rd.randint(0, 10)
-            food = Food(i, self, pos, util)
-            self.foods.append(food)
-
-
-    def new_food(self, pos):
-
-        self.n_food += 1
-        new_food = agent_type(Food, self, pos)
-        self.grid.place_agent(new_food, pos)
-        self.foods.append(new_food)
-
     def remove_food(self, food):
         '''
         Method that enables us to remove passed agents.
@@ -46,16 +31,40 @@ class HiveModel(Model):
         for food in list(self.foods):
             food.step()
 
+    def init_food(self):
+        for i in range(self.start_food):
+            pos = (rd.randrange(self.width), rd.randrange(self.height))
+            util = rd.randint(0, 10)
+            food = Food(i, self, pos, util)
+            self.foods.append(food)
+
 
 class Food(Agent):
     def __init__(self, id, model, pos, util):
         super().__init__(id, model)
+        self.id = id
+        self.model = model
         self.pos = pos
         self.util = util
 
     def step(self):
         print(f'current location: {self.pos}')
         print(f'current utility: {self.util}')
+
+
+    def new_food(self, pos):
+
+        self.n_food += 1
+        new_food = agent_type(Food, self.model, pos)
+        self.grid.place_agent(new_food, pos)
+        self.foods.append(new_food)
+
+class Bee(Agent):
+
+    def __init__(self,id, pos):
+        super().__init__(id, model)
+        self.id = id
+        self.pos = pos
 
 if __name__ == '__main__':
     FoodModel = HiveModel(10, 10)
