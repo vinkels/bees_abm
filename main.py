d49aa4a005b1b475c0970d4b3997294dc25fcf17
Tariq Mouhtadi <tariq@mouhtadi.nl>
2019-01-30 23:18:02 +0100

    put timings back in for optimization process

--- main.py
+++ main.py
@@ -15,16 +15,9 @@ def main():
     bee_model.run_model(1000)
     print(tm.time() - start_time)
 
-    print(bee_model.total_data_time, bee_model.total_schedule_time)
+    print(bee_model.total_schedule_time)
     print(bee_model.schedule.timing_by_breed)
     print(bee_model.time_by_strategy)
     print(bee_model.planning_time)
-
-    df = bee_model.datacollector.get_model_vars_dataframe()
-    df2 = bee_model.datacollector2.get_model_vars_dataframe()
-    df.to_pickle('pickles/huppelfluppel.p')
-    df.to_csv('pickles/df.csv')
-    df2.to_csv('pickles/df2.csv')
-
 
 main()
