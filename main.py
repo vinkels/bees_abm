88bea813c18127c5ee3d3f764e26c8e396fb6f0f
Tariq Mouhtadi <tariq@mouhtadi.nl>
2019-02-02 23:36:59 +0100

    Start of benchmark

--- main.py
+++ main.py
@@ -1,28 +1,52 @@
 import model as md
-import time as tm
+import time
 
 from config import GRID_WIDTH, GRID_HEIGHT
 
+from util import path_finder
+
 
 def main():
-    bee_model = md.BeeForagingModel(GRID_WIDTH, GRID_HEIGHT, 10, 30, 7)
+#     bee_model = md.BeeForagingModel(GRID_WIDTH, GRID_HEIGHT, 10, 30, 7)
 
-    for i in range(45):
-        print(f'ITERATION {i*1000}')
+    # Start at 16.0721 for 100000 iterations
 
-        print({k: len(v) for k, v in bee_model.grid.grids.items()})
-        start_time = tm.time()
-        bee_model.run_model(1000)
-        print(tm.time() - start_time)
+    import numpy as np
+    grid = np.zeros((50, 50))
+    start = (0, 0)
+    end = (5, 5)
 
-        print(bee_model.total_schedule_time)
-        print(bee_model.schedule.timing_by_breed)
-        print(bee_model.time_by_strategy)
-        print(bee_model.planning_time)
-        print(bee_model.grid.timings)
+    grid[48][48] = 1
+    for i in range(1, 47):
+        grid[48][i] = 1
+        grid[i][48] = 1
 
-        print({k: len(v) for k, v in bee_model.schedule.agents_by_breed.items()})
+    tt = 0
 
-        print(bee_model.timings_scout)
+    for _ in range(100000):
+        s = time.time()
+        path_finder(start, end, grid, 50, 50)
+        e = time.time()
+        tt += e - s
+
+    print(tt)
+
+#     for i in range(10000):
+        # print(f'ITERATION {i*1}')
+
+        # print({k: len(v) for k, v in bee_model.grid.grids.items()})
+        # start_time = tm.time()
+        # bee_model.run_model(1)
+        # print(tm.time() - start_time)
+
+        # print(bee_model.total_schedule_time)
+        # print(bee_model.schedule.timing_by_breed)
+        # print(bee_model.time_by_strategy)
+        # print(bee_model.planning_time)
+        # print(bee_model.grid.timings)
+
+        # print({k: len(v) for k, v in bee_model.schedule.agents_by_breed.items()})
+
+        # print(bee_model.timings_scout)
 
 main()
