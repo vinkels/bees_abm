19d12c6bc531f609c235f482fd61b5817c54bc93
Tariq Mouhtadi <tariq@mouhtadi.nl>
2019-02-09 13:59:53 +0100

    remove indent from scout

--- strategy/scout.py
+++ strategy/scout.py
@@ -6,36 +6,37 @@ def scout_step(bee):
     """
     This type of bee does a random walk, searching for food, and return to hive if he has found this.
     """
-    if bee.loaded is False:
-        if bee.energy < 0.5 * bee.max_energy and bee.pos != bee.hive_location:
-            bee.move(bee.hive_location)
+    assert not bee.loaded, "Scouts should be unloaded."
 
-            # check if destination is reached
-            if bee.pos == bee.hive_location:
-                bee.arrive_at_hive()
-        else:
-            food_neighbours = [
-                nb
-                for nb in bee.model.grid.get_food_neighbors(bee.pos, 1)
-                if nb.can_be_harvested()
-            ]
+    bee_is_tired = bee.energy < 0.5 * bee.max_energy
 
-            # If you see food that is uneaten, move there.
-            if food_neighbours:
-                food = random.choice(food_neighbours)
+    if bee_is_tired and bee.pos != bee.hive_location:
+        bee.move(bee.hive_location)
 
-                bee.model.grid.move_agent(bee, food.pos)
-                food.harvest()
-
-                # Become a forager take food and remember location
-                bee.type_bee = 'foraging'
-                bee.loaded = True
-                bee.food_location = bee.pos
-
-            # otherwise, move to a random neighbour
-            else:
-                neighbourhood = bee.get_accessible_neighbourhood()
-                bee.model.grid.move_agent(bee, random.choice(neighbourhood))
-
+        # check if destination is reached
+        if bee.pos == bee.hive_location:
+            bee.arrive_at_hive()
     else:
-        raise Exception("Scouts should be unloaded.")
+        food_neighbours = [
+            nb
+            for nb in bee.model.grid.get_food_neighbors(bee.pos, 1)
+            if nb.can_be_harvested()
+        ]
+
+        # If you see food that is uneaten, move there.
+        if food_neighbours:
+            food = random.choice(food_neighbours)
+
+            bee.model.grid.move_agent(bee, food.pos)
+            food.harvest()
+
+            # Become a forager take food and remember location
+            bee.type_bee = 'foraging'
+            bee.loaded = True
+            bee.food_location = bee.pos
+
+        # otherwise, move to a random neighbour
+        else:
+            neighbourhood = bee.get_accessible_neighbourhood()
+            bee.model.grid.move_agent(bee, random.choice(neighbourhood))
+
