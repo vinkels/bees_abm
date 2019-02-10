d6fc826c9003b79d8ab5e1f4f90bdf7ac6374e86
Tariq Mouhtadi <tariq@mouhtadi.nl>
2019-02-10 10:55:59 +0100

    Cleanup rester logic

--- strategy/rester.py
+++ strategy/rester.py
@@ -3,31 +3,26 @@ import random
 
 def rester_step(bee):
     """
-    This type of bee stays at the hive, until a location for food is known and then he becomes a foraging bee
+    This type of bee stays at the hive until it has regained its energy.
     """
-    # Resting bees can only be at the hive.
-    assert bee.at_hive
+    assert bee.at_hive, "Resting bees can only be at the hive."
 
     hive = bee.hive
 
-    # check if bee has enough energy for foraging
-    if bee.energy >= bee.max_energy:
-
-        # check if food locations are known
-        if hive.food_locations:
-
-            # become forager at random food location
-            bee.type_bee = "foraging"
-            bee.food_location = random.choice(hive.food_locations)
-
-        # otherwise, stay at hive and gain energy
-        else:
-            # become scout if no food has been found
-            bee.type_bee = "scout"
-
-    else:
+    if bee.energy < bee.max_energy:
         if hive.food >= hive.bite:
             bee.energy += hive.bite
             hive.food -= hive.bite
         else:
+            # Become a scout if the hive is empty
+            bee.type_bee = "scout"
+
+    else:
+        if hive.food_locations:
+            # if food locations are known, forage to a random food source.
+            bee.food_location = random.choice(hive.food_locations)
+            bee.type_bee = "foraging"
+
+        else:
+            # become scout if no food has been found before.
             bee.type_bee = "scout"
